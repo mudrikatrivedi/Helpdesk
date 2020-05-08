608f8c9890611e0c803382e89c9cf19960c9bcb6
Mudrika Trivedi <mudrikatrivedi20@gmail.com>
2020-05-08 15:31:00 +0530

    Update Login.java

--- Login.java
+++ Login.java
@@ -6,64 +6,45 @@ import javax.swing.JOptionPane;
 import java.util.*;
 import java.util.Scanner;
 public class Login{
-    public static String getMd5(String input) 
-    {
-        try { 
-            MessageDigest md = MessageDigest.getInstance("MD5"); 
-            byte[] messageDigest = md.digest(input.getBytes()); 
-            BigInteger no = new BigInteger(1, messageDigest); 
-            String hashtext = no.toString(16); 
-            while (hashtext.length() < 32) { 
-              hashtext = "0" + hashtext; 
-            } 
-            return hashtext; 
-        }  
-        catch (NoSuchAlgorithmException e) { 
-            throw new RuntimeException(e); 
-        } 
-    } 
     public static void main(String args[]) throws Exception 
 	{ 
-	Connection con;
-        PreparedStatement prepared_statement;
-        //String emp = JOptionPane.showInputDialog("EmployeeID");
-		Scanner input_string = new Scanner(System.in);
-		String employeeIDTocheck = input_string.nextLine();
-       // String pw = JOptionPane.showInputDialog("Password");
-	   java.io.Console console = System.console();
-	   //String pw=input.nextLine();
-	   String passwordTocheck = new String(console.readPassword());
-		boolean flag = false;
+	    Connection con;
+	    PreparedStatement prepared_statement;
+	    Scanner input_string = new Scanner(System.in);
+	    String empIDTocheck = input_string.nextLine();
+	    String passwordTocheck =input_string.nextLine();
+	    boolean flag = false;
+            MessageDigest md = MessageDigest.getInstance("MD5"); 
+            byte[] messageDigest = md.digest(passwordTocheck.getBytes()); 
+            BigInteger no = new BigInteger(1, messageDigest); 
+            String pwdinMD5 = no.toString(16); 
+            while (pwdinMD5.length() < 32) { 
+              pwdinMD5 = "0" + pwdinMD5; 
+            } 
     try{ 
 	    Class.forName("com.mysql.jdbc.Driver").newInstance();
-        con = (Connection) DriverManager.getConnection("jdbc:mysql://localhost:3306/sample","root","1234");
-        prepared_statement = (PreparedStatement) con.prepareStatement("select * from login where EmpID=? and Password=?");		
-	    
-        String empIDinMD5 = getMd5(employeeIDTocheck);   
-        String pwdinMD5 = getMd5(passwordTocheck);
-		prepared_statement.setString(1,empIDinMD5);
-		prepared_statement.setString(2, pwdinMD5);
-        ResultSet resultset = prepared_statement.executeQuery();
-		System.out.println("Records Exist "+resultset.next());
-		if(resultset!=null){
+            con = (Connection) DriverManager.getConnection("","","");
+            prepared_statement = (PreparedStatement) con.prepareStatement("select * from login where EmpID=? and Password=?");		  
+	    prepared_statement.setString(1,empIDTocheck);
+	    prepared_statement.setString(2, pwdinMD5);
+            ResultSet resultset = prepared_statement.executeQuery();
+	    System.out.println("Records Exist "+resultset.next());
+	    if(resultset!=null){
 		    String empIDtoCompare = resultset.getString("EmpID");
 		    String pwtoCompare = resultset.getString("Password");
-			
-		    if((empIDinMD5.equals(empIDtoCompare)) && (pwdinMD5.equals(pwtoCompare))){
+		    if((empIDTocheck.equals(empIDtoCompare)) && (pwdinMD5.equals(pwtoCompare))){
 			    flag = true;
-			   // JOptionPane.showMessageDialog(null, "Username and Password exist");
-				System.out.println("Username and password exist");
+			    System.out.println("Username and password exist");
 		    }
-		   if(!flag){
-			  //JOptionPane.showMessageDialog(null, "Please Check Again");
-			 System.out.println("Please check again");
-		   }
-	   } 
-		  resultset.close();
-con.close();
-    } catch(Exception e)
-        {
-         System.out.println(e.toString());
-        }
+		    if(!flag){
+			    System.out.println("Please check again");
+		    }
+	    } 
+	    resultset.close();
+	    con.close();
+    }
+	    catch(Exception e){
+		    System.out.println(e.toString());
+	    }
 	}
 }
